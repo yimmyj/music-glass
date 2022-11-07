77b17ebfd7222f50ee6848a92b76609127b932f1
Kevin S <firstkevinshen@gmail.com>
2022-11-06 19:55:51 -0800

    Auth testing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function requestAuth(){
+  let url = "https://accounts.spotify.com/authorize";
+  url += "?client_id=f7c40db261574146a1341b89f2d76bfb";
+  url += "&response_type=code";
+  url += "&redirect_uri=" + encodeURI("http://localhost:3000/");
+  url += "&show_dialog=true";
+  url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+  window.location.href = url; // Show Spotify's authorization screen
+}
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +27,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={requestAuth}>Request Auth</button>
       </header>
     </div>
   );
